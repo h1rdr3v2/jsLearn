d2d192d0313b068e2f7565ac605c3405681e6245
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2023-02-23 19:57:17 +0100

    str and no

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const pers = {
-	hello: "Hi bro",
-};
+let string = "Hello";
+let point = [40, 100, 1, 5, 25, 10];
+console.log(point.sort());
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
 
-const _pers = [
-	{
-		damn: "hello",
-	},
-];
-console.log(_pers[0].damn);
+function myFunction(value) {
+	txt += value + "<br>";
+}
+console.log(txt);
